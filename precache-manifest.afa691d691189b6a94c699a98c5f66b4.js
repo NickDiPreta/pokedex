b62375b8682e0daf01ab28f2dd938a4ddca48079
nick <nicholasdipreta2020@gmail.com>
2020-06-25 09:47:24 -0400

    Updates

---
+++ precache-manifest.afa691d691189b6a94c699a98c5f66b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfc04ffe75bb9e8409d2629c799630c",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "16c57b51c9993870661d",
+    "url": "/pokedex/static/css/main.1062ee0b.chunk.css"
+  },
+  {
+    "revision": "9b570917a933d7e6d44a",
+    "url": "/pokedex/static/js/2.6a9bdf1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.6a9bdf1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c57b51c9993870661d",
+    "url": "/pokedex/static/js/main.0d2068b4.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
